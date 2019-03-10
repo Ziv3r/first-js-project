320ee1d20a732566a820e96ce9ced64fedc9b6ce
ziv halsband <ziv3r123@gmail.com>
2019-03-10 10:54:13 +0200

    my first java script project

---
+++ script.js
@@ -0,0 +1,19 @@
+function turnOn() {
+
+    var image = document.getElementById('myImage').src = "https://i.imgur.com/2IRs7Bs.gif";
+}
+
+function turnOff() {
+
+    var image = document.getElementById('myImage').src = "https://i.imgur.com/8kGmXwB.png";
+}
+
+function disappear() {
+    document.getElementById('magic').style.display = 'none';
+}
+
+function show() {
+    document.getElementById('magic').style.display = 'block';
+}
+
+console.log("hello there ");
